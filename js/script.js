7db95b894d60da98f0fa86e0b4e2b54e9146c8aa
C410 <C410@AceLabs.lagcc>
2017-06-15 15:10:38 -0400

    updated css and js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //  var $mainHeader = $('#main-header');
 // $mainHeader.html('Hello');
 $('#random-gif-btn').click( function(){
-	$.get( "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=pg", function( data ) {
+	$.get( "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=pg-13", function( data ) {
   		var img= '<img src="' + data.data.image_original_url + '">';
   		$('#randam-gif-container').html(img);
   		console.log(data);
